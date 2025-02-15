8e2e120a35e2e899f372a412ba8cdbbf5a418a81
NoobZik <noobzik@pm.me>
2025-02-15 19:22:14 +0100

    Fixed source load

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apk add --no-cache gcc musl-dev linux-headers python3-dev
+apk add --no-cache gcc g++ musl-dev linux-headers python3-dev cmake ninja
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install -r requirements.txt
