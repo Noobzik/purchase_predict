6cf3e761555e5f764b5fcdc4aa4751fd68ec491b
NoobZik <noobzik@pm.me>
2025-02-15 19:13:01 +0100

    Fixed source load

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv venv --python 3.12
 source $HOME/.local/bin/env
+uv venv --python 3.12
+source .venv/bin/activate
 uv pip install -r requirements.txt
