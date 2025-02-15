4a8ca511a4f996b36eed7dcb3b0b80548f2c99d9
NoobZik <noobzik@pm.me>
2025-02-15 19:20:39 +0100

    Fixed source load

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apk add gcc p3-dev musl-dev linux-headers
+apk add --no-cache gcc musl-dev linux-headers python3-dev
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install -r requirements.txt
