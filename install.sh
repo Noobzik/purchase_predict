bec1b1cb105d02a297f391c3d7316786aa43f68f
NoobZik <noobzik@pm.me>
2025-02-15 19:27:41 +0100

    Fixed source load

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
-apk add --no-cache \
-    gcc \
-    g++ \
-    musl-dev \
-    linux-headers \
-    python3-dev \
-    cmake \
-    ninja \
-    make \    # Added make
-    git \     # Added git for some Python packages that might need it
-    libressl-dev \  # Added SSL dev files
-    libffi-dev     # Added libffi for some Python dependencies
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install -r requirements.txt
